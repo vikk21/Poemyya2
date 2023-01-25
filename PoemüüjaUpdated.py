9a10899364d10f19c64794e186e9582677b191cd
vikk21 <99194533+vikk21@users.noreply.github.com>
2023-01-25 21:45:39 +0200

    Update PoemüüjaUpdated.py

--- PoemüüjaUpdated.py
+++ PoemüüjaUpdated.py
@@ -1,35 +1,42 @@
 import pygame
 pygame.init()
 
-#ekraani seaded
-screen=pygame.display.set_mode([640,480]) #Ekraani laius
-pygame.display.set_caption("Ülesanne 2") #Ekraani nimi
-screen.fill([204, 255, 204]) #Ekraani täitmine
+#Ekraani seaded
+screen=pygame.display.set_mode([640,480]) #annab resulutsiooni loodud aknale
+pygame.display.set_caption("Ülessanne 2") #nimetab akna
+screen.fill([204, 255, 204]) #taustavärv
 
-#Lisame pildid
-shop = pygame.image.load("image/bg_shop.jpg") #poe taustapilt
-chat = pygame.image.load("image/chat.png") #chat pilt
-seller = pygame.image.load("image/seller.png") #müüja pilt
+#Piltide kasutamine failidest
+bg_shop = pygame.image.load("img/bg_shop.jpg")
+seller = pygame.image.load("img/seller.png")
+chat = pygame.image.load("img/chat.png")
+vikklogo = pygame.image.load("img/vikklogo.png")
+mõõk = pygame.image.load("img/mõõk.png")
+tort = pygame.image.load("img/tort.png")
+#Piltide suurused
+bg_shop = pygame.transform.scale(bg_shop, [640, 480])
 seller = pygame.transform.scale(seller, [200, 240]) #müüja suurus
 chat = pygame.transform.scale(chat, [200, 150]) #chat suurus
-screen.blit(shop,[0,0]) #pildi kordinaadid
-screen.blit(seller,[175,170]) #pildi kordinaadid
-screen.blit(chat,[340,120]) #pildi kordinaadid
-#Text
-font = pygame.font.Font(None, 30) #Fonti sätted
+vikklogo = pygame.transform.scale(vikklogo, [300, 50])
+mõõk = pygame.transform.scale(mõõk, [100, 100])
+tort = pygame.transform.scale(tort, [100, 100])
+#Piltide kordinaadid
+screen.blit(bg_shop,[0,0])
+screen.blit(seller,[175,170])
+screen.blit(chat,[340,120])
+screen.blit(vikklogo,[0,0])
+screen.blit(mõõk,[480,180])
+screen.blit(tort,[350,190])
+pygame.display.flip() #Värskendab akent
+font = pygame.font.Font(None, 30) #Annab tekstile fondi ning selle suuruse
 text = font.render("Tere Toomas!", True, [255,255,255]) #Teksti sisu ja värv
-screen.blit(text, [375,175])
-
-
-pygame.display.flip() #värskendab aknaid
-running = True #Hoiab programmi töös
-
-#Sulgeb akna punasest ristist
-while running: #Loop
-    # Check for event if user has pushed
-    # any event in queue
-    for event in pygame.event.get(): # Alustab programm mis paneb akna kinni
-        # if event is of type quit then set
-        # running bool to false
-        if event.type == pygame.QUIT:
-            running = False
+screen.blit(text, [375,175]) #Vaatab mis koodrinaatidele on see tekst kirjutatud
+pygame.draw.rect(screen, [255, 255, 255], [0, 0, 300, 50], 2) #Joonistab vikk logo ümber ristküliku
+pygame.draw.arc(screen,[255,255,255], [240,0,80,50], -3.14/3, 1) #Joonistab kaare vikklogo juurde joonistatud ristküliku kõrvale
+pygame.display.flip() #Värskendab akent
+#Alustab tsükliga
+run = True
+while run: #teeb tsükli
+    for event in pygame.event.get(): #kui alustatakse programm siis alustatakse pygame
+        if event.type == pygame.QUIT: #kui programm pannakse kinni läheb ka pygame kinni
+            run = False
